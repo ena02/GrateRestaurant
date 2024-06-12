9f9304f937fabe894860e1dd10bc00366f27e41e
ena <erkembekov.nurzhan@mail.ru>
2024-06-12 18:08:03 +0500

    Add geting products by id and geting all products

---
+++ Greate.Services.ProdustAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,52 @@
+﻿using Greate.Services.ProdustAPI.Models.Dto;
+using Greate.Services.ProdustAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greate.Services.ProdustAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : Controller
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            this._response = new ResponseDto();
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetAllProductsAsync();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductByIdAsync(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+            }
+            return _response;
+        }
+    }
+}
