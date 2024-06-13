1e1e92107f3f1d3d8ad12fbffc0387b131d63b3e
ena <erkembekov.nurzhan@mail.ru>
2024-06-14 03:43:03 +0500

    Add ProductDto for web, add product Interface

---
+++ Grate.web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Greate.web.Models;
+
+namespace Grate.web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
