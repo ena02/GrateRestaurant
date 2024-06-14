9da95623c302ff7210ccec7d43ee7fe3ff0f16f1
ena <erkembekov.nurzhan@mail.ru>
2024-06-14 07:46:10 +0500

    Made service for sending http request

---
+++ Grate.web/Services/BaseService.cs
@@ -0,0 +1,76 @@
+﻿using Grate.web.Models;
+using Grate.web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Grate.web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel {  get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDto();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("GreateAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.ApiUrl);
+                client.DefaultRequestHeaders.Clear();
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), 
+                        Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch (Exception ex) 
+            {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponse =JsonConvert.DeserializeObject<T>(res);
+                return apiResponse;
+            }
+                
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
