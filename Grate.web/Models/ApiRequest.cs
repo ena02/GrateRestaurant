4c8ee3003de7848560ae3b4ee0184849c5409405
ena <erkembekov.nurzhan@mail.ru>
2024-06-14 07:20:41 +0500

    Add api request type

---
+++ Grate.web/Models/ApiRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Grate.web.SD;
+
+namespace Grate.web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string ApiUrl { get; set; } 
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
