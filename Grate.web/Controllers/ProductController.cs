1009e85f8f60e79fee3d37b48f66444c559ba902
ena <erkembekov.nurzhan@mail.ru>
2024-07-08 09:00:03 +0500

    add product controller

---
+++ Grate.web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Grate.web.Models;
+using Grate.web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Grate.web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess) 
+            { 
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View();
+        }
+    }
+}
