33e0ac7a3541e3e957c2a4228f8c5af0012eb9bb
ena02 <erkembekovn@gmail.com>
2024-10-20 19:55:02 +0500

    change db setting

---
+++ Greate.Services.ProdustAPI/Migrations/20241020133003_AddProductModelToDb2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Greate.Services.ProdustAPI.Migrations
+{
+    public partial class AddProductModelToDb2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
