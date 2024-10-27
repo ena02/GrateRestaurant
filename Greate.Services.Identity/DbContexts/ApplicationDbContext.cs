a39031242645d0192dd4fbbde1c1fc00817d967b
ena02 <erkembekovn@gmail.com>
2024-10-28 01:11:30 +0500

    add identity service

---
+++ Greate.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Greate.Services.Identity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Greate.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApllicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Greate.Services.Identity/Models/ApllicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Greate.Services.Identity.Models
+{
+    public class ApllicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
